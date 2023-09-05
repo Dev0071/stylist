1f895d8156f5f715938f71932fc76dd24737635b
Dev0071 <lecturergacheru71@gmail.com>
2023-09-05 09:08:57 +0300

    registeration form

---
+++ server/db/dbconfig.js
@@ -0,0 +1,30 @@
+import mssql from 'mssql';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new mssql.ConnectionPool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PWD,
+	database: process.env.DB_NAME,
+	server: 'DEV007\\MSSQLSERVER1',
+	pool: {
+		max: 10,
+		min: 0,
+		idleTimeoutMillis: 30000,
+	},
+	options: {
+		encrypt: false,
+		trustServerCertificate: false,
+	},
+});
+
+const connectToPool = async () => {
+	try {
+		await pool.connect();
+		console.log('connected to db...');
+	} catch (error) {
+		console.log('error connecting to database pool', error.message);
+	}
+};
+
+export { connectToPool, pool };
